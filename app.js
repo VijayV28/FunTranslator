a7ec6eeee93d316df691982aa4ecdade6f06c08e
VijayV28 <81868201+VijayV28@users.noreply.github.com>
2022-01-18 10:56:11 +0530

    update: Input received and output displayed

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var translateBtn = document.querySelector("#btn-translate");
+var inputSection = document.querySelector("#txtbox-input");
+var outputSection = document.querySelector("#txtbox-output");
 
+console.log(translateBtn)
 
 translateBtn.addEventListener("click",buttonClicked);
 function buttonClicked(){
     console.log("Clicked");
+    var input = console.log("input: " + inputSection.value)
+    outputSection.innerText = "Welcome " + inputSection.value;
 }
+
+
 
